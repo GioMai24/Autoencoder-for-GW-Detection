bf486c3d0981970f643b8fbf36bcb224b22d3cc9
BelliLuigi <lbelli.718@gmail.com>
2025-12-09 21:03:17 +0000

    fixed h5dataset.py, it's very important that we have same version bc otherwise saved idxs won't work. Implemented moreno's way to deal with 100 timestep. Did AE of paper, doing training phase.

--- h5dataset.py
+++ h5dataset.py
@@ -1,28 +1,24 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
+from torch.utils.data import Dataset
 import h5py as h5
-import os
 
-
-class h5set(torch.utils.data.Dataset):
+class h5set(Dataset):
     def __init__(self, path, dataset='noise'):
         super().__init__()
         self.path = path
+        self.name = dataset
         self.dataset = None
-        with h5.File(name=self.path,mode="r", libver="latest", locking=False) as f:
+        with h5.File(path, mode="r", libver="latest", locking=False) as f:
             self.length = len(f[dataset])
 
     def __len__(self):
         return self.length
     
     def __getitem__(self, idx):
-        if self.dataset==None:
-            self.dataset= h5.File(self.path, 'r', libver='latest', locking=False)
-        item = self.dataset[dataset][idx]
+        if self.dataset is None:
+            self.dataset = h5.File(self.path, 'r', libver='latest', locking=False)
+        item = self.dataset[self.name][idx]
         return torch.from_numpy(item)
-
-
 
 ## 325k noise --> 25k test, 75k val, 225k train
 # 25k test
