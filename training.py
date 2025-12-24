24f4f5c3e220705d1c6e1fbe9f524d35714e371a
BelliLuigi <lbelli.718@gmail.com>
2025-12-24 08:28:12 +0000

    hehe, I just wasted a whole night for an mistake

--- training.py
+++ training.py
@@ -2,7 +2,7 @@ from h5dataset import h5set
 import os
 import torch 
 import torch.nn as nn
-from torch.utils.data import DataLoader, ConcatDataset
+from torch.utils.data import DataLoader
 import pickle
 # custom
 from automobili import EricGio
@@ -12,8 +12,11 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 print(f'Selected device: {device}')
 
 # data = h5set('/mnt/eph/data/l1500mb_c.h5', 100, 30)
-data = torch.load("/mnt/eph/data/train_test_val/train.pt", weights_only=False)
-training = DataLoader(data, batch_size=1, num_workers=os.cpu_count())
+data_train = torch.load("/mnt/eph/data/train_test_val/train.pt", weights_only=False)
+training = DataLoader(data_train, batch_size=1, num_workers=os.cpu_count())
+
+data_val = torch.load("/mnt/eph/data/train_test_val/val.pt", weights_only=False)
+validation = DataLoader(data_val, batch_size=1, num_workers=os.cpu_count())
 
 AE = EricGio()
 AE.to(device)
@@ -35,6 +38,6 @@ for epoch in range(1, num_epochs + 1):
     if epoch % 10 == 0 or epoch == num_epochs: 
             torch.save({'model_state_dict': model.state_dict(),
                     'optimizer_state_dict': optimizer.state_dict()
-                    }, f'/mnt/runs/model_optim_weights/model_optim_{epoch}.pt')
+                    }, f'/mnt/eph/runs/model_optim_weights/model_optim_{epoch}.pt')
 
-    with open(f'/mnt/runs/losses/loss_{epoch}.pickle', 'wb') as fout: pickle.dump(losses, fout, protocol=-1)
+    with open(f'/mnt/eph/runs/losses/loss_{epoch}.pickle', 'wb') as fout: pickle.dump(losses, fout, protocol=-1)
