d035d227addd8f4ca8b1433992ff4d47a1a6251a
Il tuo nome <tu@esempio.com>
2026-01-08 14:24:10 +0100

    began train

--- training.py
+++ training.py
@@ -5,18 +5,67 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import pickle
 # custom
-from automobili import EricGio
-from funkytrain import traingio, evalgio
+import custom.tools as tl
+import custom.models as mod
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-print(f'Selected device: {device}')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    torch.backends.cudnn.benchmark = True
+else: device = torch.device('cpu')
+print(f'{device=}')
 
-# data = h5set('/mnt/eph/data/l1500mb_c.h5', 100, 30)
-data_train = torch.load("/mnt/eph/data/train_test_val/train.pt", weights_only=False)
-training = DataLoader(data_train, batch_size=1, num_workers=os.cpu_count())
+last_save = torch.load('/mnt/eph/data/optuna/artifacts/ba4a7fec-42c2-4b9f-a9cb-bc4f448671b1', weights_only=False)
+## Useful parameters:
+batch_size = 64
+last_epoch = 30
+num_epochs = 30
+win_len = 100
+win_stride = 60
+num_workers = 10
+persistent_workers = True if num_workers>0 else False
 
-data_val = torch.load("/mnt/eph/data/train_test_val/val.pt", weights_only=False)
-validation = DataLoader(data_val, batch_size=1, num_workers=os.cpu_count())
+h5_path = "/mnt/eph/data/L1_70_12_6_6.h5"
+train = tl.h5set(h5_path, win_len=win_len, win_stride=win_stride, name='A')
+val = tl.h5set(h5_path, win_len=win_len, win_stride=win_stride, name='B')
+
+train_loader = DataLoader(train, batch_size=batch_size, pin_memory=True, num_workers=num_workers, persistent_workers=persistent_workers)
+val_loader = DataLoader(val, batch_size=batch_size, pin_memory=True, num_workers=num_workers, persistent_workers=persistent_workers)
+
+loss_func = nn.MSELoss()
+model = mod.LSTMAEUni().to(device)
+model = torch.compile(model)
+model.load_state_dict(last_save['model_state_dict'])
+
+# Trial choices
+optim_name = 'NAdam'
+lr = 0.0009419995643886475
+clip = 4.964217914740257
+decay = 0
+if optim_name == 'SGD':
+    momentum = 0
+    nesterov = False
+    optim = getattr(op, optim_name)(model.parameters(), lr=lr, momentum=momentum, nesterov=nesterov, weight_decay=decay)
+elif optim_name == 'RMSprop':
+    momentum = 0
+    optim = getattr(op, optim_name)(model.parameters(), lr=lr, momentum=momentum, weight_decay=decay)
+else: optim = getattr(op, optim_name)(model.parameters(), lr=lr, weight_decay=decay)
+optim.load_state_dict('optimizer_state_dict')
+
+losses = last_save['losses']
+scaler = GradScaler()
+scaler.load_state_dict('scaler_state_dict')
+for epoch in range(last_epoch + 1, num_epochs + 1):
+    train_loss = tl.train_epoch(model, device, train_loader, loss_func, optim, scaler, clip, False)
+    val_loss = tl.val_epoch(model, device, val_loader, loss_func, False)
+    print(f'TRAIN - EPOCH {epoch}/{num_epochs} - loss: {train_loss} - test loss: {val_loss}')
+    losses['train'].append(train_loss)
+    losses['val'].append(val_loss)
+
+save_path = f'{main_dir}checkpoint.pt'
+tl.save_everything(model, optim, train_loader, val_loader, scaler, losses, epoch, clip, save_path)
+
+
+
 
 model = EricGio()
 model.to(device)
